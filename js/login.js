2600a69623c4c75b17ae991accd30d08a34b2a84
Luis Toledo <luistoledosv@gmail.com>
2022-12-31 00:36:48 -0300

    New UI

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 
-const form = document.querySelector("#loginForm");
+const form = document.querySelector(".loginForm");
 form.addEventListener("submit", event => {
     event.preventDefault();
 
@@ -8,9 +8,24 @@ form.addEventListener("submit", event => {
 
     const storedUser = JSON.parse(localStorage.getItem("user"));
     if (storedUser && storedUser.email === email && storedUser.password === password) {
-        alert("Bem-vindo de volta!");
-        window.location.href = '/Users/luistoledo/Desktop/ChatGPT Projetos/Consumo de Combustivel v3/main.html';
+        const okMessage = document.getElementById('okMessage');
+        okMessage.style.display = 'flex';
+
+        // 2seg para expirar a mensagem de sucesso
+        setTimeout(function () {
+            mensagemSucesso.style.display = 'none';
+        }, 2000);
+
+        // 2.5seg para redirecionar o usuário 
+        setTimeout(function () {
+            window.location.href = '/Users/luistoledo/Desktop/ChatGPT Projetos/Consumo de Combustivel v3/main.html';
+        }, 2500);
+
     } else {
-        alert("Email ou senha incorretos.");
+        const errorMessage = document.getElementById('errorMessage');
+
+        // Quando o usuário ou senha estão incorretos, remova o atributo "display: none"
+        // da mensagem de erro para exibi-la
+        errorMessage.style.display = 'flex';
     }
 });
--- js/register.js
+++ js/register.js
@@ -1,10 +1,17 @@
-const registerForm = document.querySelector("#registerForm");
+const registerForm = document.querySelector(".registerForm");
 registerForm.addEventListener("submit", function (event) {
     event.preventDefault(); // evita que a página recarregue ao submeter o formulário
 
     const name = registerForm.elements.name.value;
     const email = registerForm.elements.email.value;
     const password = registerForm.elements.password.value;
+
+    // verifica se todos os campos foram preenchidos
+    if (!name || !email || !password) {
+        const errorMessage = document.getElementById('errorMessage'); // busca o alerta de error
+        errorMessage.style.display = 'flex'; // faz o alerta aparecer
+        return; // interrompe o script
+    }
 
     const user = {
         name: name,
@@ -15,9 +22,12 @@ registerForm.addEventListener("submit", function (event) {
     // adiciona o usuário ao localStorage
     localStorage.setItem("user", JSON.stringify(user));
 
-    function cadastrar() {
-        // código para cadastrar o usuário
-        alert('Usuário cadastrado com sucesso!');
-    }
+    // exibe a mensagem de sucesso
+    const okMessage = document.getElementById('okMessage'); // busca o alerta de sucesso
+    okMessage.style.display = 'flex'; // faz o alerta aparecer
 
+    // 1.5seg para redirecionar o usuário 
+    setTimeout(function () {
+        window.location.href = '/Users/luistoledo/Desktop/ChatGPT Projetos/Consumo de Combustivel v3/index.html';
+    }, 1500);
 });
